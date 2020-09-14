47df204d7dec43de242b2dd9b0c290bed3a36b17
Yang Zihan <yang.zihan@columabi.edu>
2020-09-14 18:11:19 +0800

    demo

---
+++ sample/target.js
@@ -0,0 +1,15 @@
+
+console.log('\nThis is the sample code for MNL.js\n');
+
+let myHeart = ['kind', 'evil', 'happy'];
+
+console.log(`I born with ${myHeart.join(', ')}.`);
+
+remove__x__from('evil',myHeart);
+
+console.log(`Now, I have only ${myHeart.join(', ')} in my heart.\n`);
+
+function remove__x__from(item,array) {
+    array.splice(array.indexOf(item), 1);
+    console.log(`Now I removed the ${item} in it.`);
+}
